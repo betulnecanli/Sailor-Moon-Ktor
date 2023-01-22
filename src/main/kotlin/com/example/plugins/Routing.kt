afdfff50610c0f6b55ef9c271638d22a29891108
betulnecanli <betulnecanli@gmail.com>
2023-01-22 18:25:30 +0300

    Routing updated

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -5,10 +5,14 @@ import com.example.routes.root
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 
 fun Application.configureRouting() {
     routing {
        root()
         characters()
+        static("/images") {
+            resources("/images")
+        }
     }
 }
